9d38f080d7562756754bd99ef50b004d3fc4925f
Egor <berezin@rocketmail.com>
2026-02-12 00:10:10 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'hostel-manager-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    'https://unpkg.com/docx@7.1.0/build/index.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .catch(() => {
+                        // Если офлайн и нет в кэше - возвращаем заглушку
+                        if (event.request.destination === 'document') {
+                            return caches.match('/index.html');
+                        }
+                    });
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
